e8b2189e0e9bc9074d73306a5f40ff779b9216e9
Yurii Osadchyi <yurii.osadchyi@akvelon.com>
2020-05-05 16:53:03 +0300

    added v-app wrapper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,3 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-btn small color="primary">Primary</v-btn>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
