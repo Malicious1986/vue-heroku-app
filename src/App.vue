127f4d02363bbe703264b6c5b83bdcd44787ac78
Yurii Osadchyi <yurii.osadchyi@akvelon.com>
2020-05-05 12:50:36 +0300

    added test key

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">{{process.env.testKey}}</router-link> |
     </div>
     <router-view/>
   </div>
